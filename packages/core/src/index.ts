b930c29237dce9c5d0fb31f86483ba885e8c0e4a
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-09-25 14:34:41 +0200

    ✨ Add getIsTouch

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,2 +1,3 @@
-export {request} from "./utils/request/request"
-export type {RequestResult, RequestOptions, ApiFunctionResult} from "./types/request"
+export { request } from './utils/request/request';
+export type { RequestResult, RequestOptions, ApiFunctionResult } from './types/request';
+export { getIsTouch } from './utils/is-touch/isTouch';
---
+++ packages/core/src/utils/is-touch/isTouch.ts
@@ -0,0 +1,1 @@
+export const getIsTouch = () => !matchMedia('(pointer:fine)').matches;
