92c46ce46e66b49fc86d3c99b43f7be2e8b67170
Michael Gesenhues <michael.gesenhues@tobit.com>
2024-09-25 14:35:16 +0200

    ✨ Add getIsTobitEmployee

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 export { request } from './utils/request/request';
 export type { RequestResult, RequestOptions, ApiFunctionResult } from './types/request';
 export { getIsTouch } from './utils/is-touch/isTouch';
+export { getIsTobitEmployee } from './utils/is-tobit-emloyee/isTobitEmployee';
